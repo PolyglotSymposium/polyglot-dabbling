30a36b39b6b1b5a5ecb1a157c32946cd366f9bbf
Kazark <kazark@zoho.com>
2014-11-15 15:17:13 -0500

    Living cell with two live neighbors lives

--- haskell/conway-game-of-life/LifeAndDeath.hs
+++ haskell/conway-game-of-life/LifeAndDeath.hs
@@ -2,4 +2,5 @@ module LifeAndDeath where
 
 data Cell = DeadCell | LivingCell deriving (Eq, Show)
 
+nextStateOf _ 2 = LivingCell
 nextStateOf _ _ = DeadCell
--- haskell/conway-game-of-life/LifeAndDeathSpec.hs
+++ haskell/conway-game-of-life/LifeAndDeathSpec.hs
@@ -8,3 +8,5 @@ main = hspec $ do
     describe "A cell which was alive in the previous generation" $do
         it "should be dead now if it only had one living neighbor" $do
             nextStateOf LivingCell 1 `shouldBe` DeadCell
+        it "should still be living if it had two living neighbors" $do
+            nextStateOf LivingCell 2 `shouldBe` LivingCell
