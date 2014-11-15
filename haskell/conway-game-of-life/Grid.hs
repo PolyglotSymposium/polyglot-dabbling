3df673e81c880d87b51543aaf973333afb7125c0
Kazark <kazark@zoho.com>
2014-11-15 17:21:47 -0500

    Added "top as list" as I continue to explore grid

--- haskell/conway-game-of-life/Grid.hs
+++ haskell/conway-game-of-life/Grid.hs
@@ -4,12 +4,14 @@ data LinkedList2D a = Node | Terminator deriving (Eq, Show)
 
 empty = Terminator
 
+topAsList _ = [1]
+
 fromRowLists _ = [[1, 2], [2, 4]]
 
 pushAside _ _ = [[1], [2], [3]]
 
 pushDown _ _ = [1, 2, 3]
 
-innerZip _ _ = empty
+innerZipWith _ _ _ = empty
 
 listRows grid = grid
--- haskell/conway-game-of-life/GridSpec.hs
+++ haskell/conway-game-of-life/GridSpec.hs
@@ -7,9 +7,11 @@ main :: IO()
 main = hspec $ do
     describe "Given a two-dimensional grid, singly-linked in each direction" $do
         context "which is empty, when I push a list onto its side" $do
+            let grid = pushAside empty [1, 2, 3]
             it "should have one column" $do
-                let grid = pushAside empty [1, 2, 3]
                 listRows grid `shouldBe` [[1], [2], [3]]
+            it "should have a top row equal to the first element of the list" $do
+                topAsList grid `shouldBe` [1]
         context "which is empty, when I push a list onto its top" $do
             it "should have one column" $do
                 let grid = pushDown empty [1, 2, 3]
@@ -20,4 +22,10 @@ main = hspec $ do
                 listRows grid `shouldBe` [[1, 2], [2, 4]]
         context "which is empty, when I inner-zip a grid that is not empty onto it" $do
             it "should produce an empty grid" $do
-                innerZip empty (fromRowLists [[2]]) `shouldBe` empty
+                innerZipWith (+) empty (fromRowLists [[2]]) `shouldBe` empty
+        context "which is not empty, when I inner-zip a grid that is the same size onto it" $do
+            it "should produce a combination of the two grids" $do
+                pending
+                --let grid1 = fromRowLists [[True, False], [False, False]]
+                --let grid2 = fromRowLists [[False, False], [False, True]]
+                --innerZipWith (||) grid1 grid2 `shouldBe` fromRowLists [[True, False], [False, True]]
