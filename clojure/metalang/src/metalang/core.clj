7f76770600c4065f85995d761396bcb5a5bdda24
Michael Gilliland <mjg.py3@gmail.com>
2014-11-21 22:17:34 -0500

    Some work on parameterized things (functions and calls)

--- clojure/metalang/src/metalang/core.clj
+++ clojure/metalang/src/metalang/core.clj
@@ -3,14 +3,25 @@
 (defmacro metalang [code]
   `(quote ~code))
 
-(defn translate [_ code]
-  (if ((complement list?) code)
+(defn comma-sep [values]
+  (clojure.string/join ", " values))
+
+(defn translate-ruby [code]
+  (if (not (list? code))
     code
     (let [f (first code)] 
       (cond
-        (= 'anon f)
-          "-> {}"
+        (or (= 'anon f) (= 'λ f))
+          (str "->"
+               (let [params (nth code 1)]
+                 (if (empty? params) "" (str "(" (comma-sep params) ")")))
+               "{}")
         (= 'define f)
-          (str (nth code 1) " = " (translate :ruby (nth code 2)))
+          (str (nth code 1) " = " (translate-ruby (nth code 2)))
         (= 'call f)
-          (str "(" (translate :ruby (nth code 1)) ").()")))))
+          (str "(" (translate-ruby (nth code 1)) ").(" (comma-sep (nth code 2)) ")")))))
+
+(def translator-of { :ruby translate-ruby })
+
+(defn translate [lang code]
+  ((translator-of lang) code))
--- clojure/metalang/test/metalang/core_test.clj
+++ clojure/metalang/test/metalang/core_test.clj
@@ -6,7 +6,16 @@
   (is (= '(a b (c)) (metalang (a b (c))))))
 
 (deftest ruby-translate-of-metalang-anon-returns-a-stabby-proc
-  (is (= "-> {}" (translate :ruby (metalang (anon [] ()))))))
+  (is (= "->{}" (translate :ruby (metalang (anon [] ()))))))
+
+(deftest ruby-translate-of-metalang-λ-returns-a-stabby-proc
+  (is (= "->{}" (translate :ruby (metalang (λ [] ()))))))
+
+(deftest ruby-translate-of-metalang-λ-2
+  (is (= "->(a){}" (translate :ruby (metalang (λ [a] ()))))))
+
+(deftest ruby-translate-of-metalang-λ-3
+  (is (= "->(a, b){}" (translate :ruby (metalang (λ [a b] ()))))))
 
 (deftest ruby-translate-of-define-returns-assignment
   (is (= "answer = 42" (translate :ruby (metalang (define answer 42))))))
@@ -18,7 +27,13 @@
   (is (= "the_answer = 99" (translate :ruby (metalang (define the_answer 99))))))
 
 (deftest ruby-translate-of-define-4
-  (is (= "a = -> {}" (translate :ruby (metalang (define a (anon [] ())))))))
+  (is (= "a = ->{}" (translate :ruby (metalang (define a (anon [] ())))))))
 
 (deftest ruby-call
-  (is (= "(-> {}).()" (translate :ruby (metalang (call (anon [] ()) []))))))
+  (is (= "(->{}).()" (translate :ruby (metalang (call (anon [] ()) []))))))
+
+(deftest ruby-call-2
+  (is (= "(->{}).(a)" (translate :ruby (metalang (call (anon [] ()) [a]))))))
+
+(deftest ruby-call-3
+  (is (= "(->{}).(a, b)" (translate :ruby (metalang (call (anon [] ()) [a b]))))))
