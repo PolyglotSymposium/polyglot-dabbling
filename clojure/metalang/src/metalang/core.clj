66d738d1a7e33f455ce9e96e851ff64801dd093b
Michael Gilliland <mjg.py3@gmail.com>
2014-11-21 21:30:53 -0500

    Added basic translator, first steps

--- clojure/metalang/src/metalang/core.clj
+++ clojure/metalang/src/metalang/core.clj
@@ -2,3 +2,6 @@
 
 (defmacro metalang [code]
   `(quote ~code))
+
+(defn translate [_ code]
+  "-> {}")
--- clojure/metalang/test/metalang/core_test.clj
+++ clojure/metalang/test/metalang/core_test.clj
@@ -4,3 +4,6 @@
 
 (deftest metalang-just-returns-its-input-but-quoted
   (is (= '(a b (c)) (metalang (a b (c))))))
+
+(deftest ruby-translate-of-metalang-anon-returns-a-stabby-proc
+  (is (= "-> {}" (translate :ruby (metalang (anon))))))
