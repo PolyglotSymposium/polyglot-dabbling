5ffb0e80db9372e534297749627fad1029447161
Michael Gilliland <mjg.py3@gmail.com>
2014-11-28 10:34:30 -0500

    Added CSS style

--- ruby/html/html.rb
+++ ruby/html/html.rb
@@ -7,12 +7,18 @@ def method_missing(tag, attrs = {})
   "<#{tag}#{str_attrs}" + (block_given? ? ">\n#{yield}\n</#{tag}>" : " />\n")
 end
 
+class Hash
+  def to_css
+    reduce('') { |mem, (k, v)| "#{mem}#{k}: #{v};" }
+  end
+end
+
 puts html {
   head {
     title { 'Test page' }
   } +
     body {
-    h1(color: :red) { 'this is a test' } +
+    h1(style: {color: :red, 'font-style' => :italic}.to_css) { 'this is a test' } +
     br +
     p { 'This is a test' }
   }
