0dc39b163a6e415fd8499369fedd557b830f452c
Michael Gilliland <mjg.py3@gmail.com>
2015-11-18 17:56:26 -0500

    Singleton zipper

--- haskell/zipper_examples/ZipperExamples.hs
+++ haskell/zipper_examples/ZipperExamples.hs
@@ -6,14 +6,20 @@ import Test.QuickCheck
 data ListZipper a =
   Something
   | EmptyZipper
+  | Zipper [a] a [a]
   deriving (Show, Eq)
 
 fromList :: [a] -> ListZipper a
-fromList = const EmptyZipper
+fromList [] = EmptyZipper
+fromList [v] = Zipper [] v []
 
 main = hspec $ do
   describe "ListZipper" $ do
     describe "fromList" $ do
       describe "given an empty list" $ do
         it "returns the EmptyZipper" $ do
-          fromList [] `shouldBe` EmptyZipper
+          fromList [] `shouldBe` (EmptyZipper :: ListZipper Int)
+
+      describe "given a singleton list" $ do
+        it "returns a proper zipper with empty sides" $ do
+          fromList [42] `shouldBe` Zipper [] 42 []
