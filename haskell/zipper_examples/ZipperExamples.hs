d4a3655d0a23f944d70fc5f8afc655a03e380b72
Michael Gilliland <mjg.py3@gmail.com>
2015-11-18 17:49:49 -0500

    Add EmptyZipper

--- haskell/zipper_examples/ZipperExamples.hs
+++ haskell/zipper_examples/ZipperExamples.hs
@@ -3,13 +3,17 @@ module ZipperExamples where
 import Test.Hspec
 import Test.QuickCheck
 
-data ListZipper a = Something
+data ListZipper a =
+  Something
+  | EmptyZipper
   deriving (Show, Eq)
 
 fromList :: [a] -> ListZipper a
-fromList = const Something
+fromList = const EmptyZipper
 
 main = hspec $ do
   describe "ListZipper" $ do
-    it "exists" $ do
-      fromList [] `shouldBe` fromList []
+    describe "fromList" $ do
+      describe "given an empty list" $ do
+        it "returns the EmptyZipper" $ do
+          fromList [] `shouldBe` EmptyZipper
