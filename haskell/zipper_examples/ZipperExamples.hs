3dd88a3d2aa219c133c8f8bc94b9c68a9b894c1e
Michael Gilliland <mjg.py3@gmail.com>
2015-11-18 20:15:01 -0500

    Some of move

--- haskell/zipper_examples/ZipperExamples.hs
+++ haskell/zipper_examples/ZipperExamples.hs
@@ -9,6 +9,15 @@ data ListZipper a =
   | Zipper [a] a [a]
   deriving (Show, Eq)
 
+data Movement = LeftBy Int | RightBy Int
+
+move :: ListZipper a -> Movement -> ListZipper a
+move EmptyZipper _ = EmptyZipper
+move zipper (LeftBy 0) = zipper
+move zipper (RightBy 0) = zipper
+move zipper@(Zipper [] x xs) (LeftBy _) = zipper
+move zipper@(Zipper xs x []) (RightBy _) = zipper
+
 fromList :: [a] -> ListZipper a
 fromList [] = EmptyZipper
 fromList (x:xs) = Zipper [] x xs
@@ -17,8 +26,48 @@ insertFront :: a -> ListZipper a -> ListZipper a
 insertFront value EmptyZipper = Zipper [] value []
 insertFront value (Zipper start focus rest) = Zipper (start ++ [value]) focus rest
 
+arbitrary_non_empty_zipper :: Gen (ListZipper Int)
+arbitrary_non_empty_zipper = do
+  start <- arbitrary
+  rest <- arbitrary
+  focus <- arbitrary
+  return $ Zipper start focus rest
+
+main :: IO ()
 main = hspec $ do
   describe "ListZipper" $ do
+    describe "move" $ do
+      describe "to the left" $ do
+        describe "by 0" $ do
+          it "does not modify the zipper" $ do
+           property $ forAll arbitrary_non_empty_zipper $ \zipper -> move zipper (LeftBy 0) == zipper
+
+        describe "by any amount on the empty zipper" $ do
+          it "does not modify the zipper" $ do
+           property $ \n ->
+             n >= 0 ==> move (EmptyZipper :: ListZipper Int) (LeftBy n) == EmptyZipper
+
+        describe "by any amount, on a zipper focusing on the first element" $ do
+          it "does not modify the zipper" $ do
+           property $ \x xs n ->
+             n > 0 ==> move (Zipper [] (x :: Int) xs) (LeftBy n) == Zipper [] x xs
+
+      describe "to the right" $ do
+        describe "by 0" $ do
+          it "does not modify the zipper" $ do
+           property $ forAll arbitrary_non_empty_zipper $ \zipper -> move zipper (RightBy 0) == zipper
+
+        describe "by any amount on the empty zipper" $ do
+          it "does not modify the zipper" $ do
+           property $ \n ->
+             n >= 0 ==> move (EmptyZipper :: ListZipper Int) (RightBy n) == EmptyZipper
+
+        describe "by any amount, on a zipper focusing on the final element" $ do
+          it "does not modify the zipper" $ do
+           property $ \x xs n ->
+             n > 0 ==> move (Zipper xs (x :: Int) []) (RightBy n) == Zipper xs x []
+
+
     describe "insertFront" $ do
       describe "into empty" $ do
         it "returns a singleton zipper" $ do
