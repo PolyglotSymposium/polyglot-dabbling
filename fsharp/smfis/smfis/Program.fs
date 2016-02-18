62cb19aff9a6c7b60ead4663278ed3b582e3b6d0
Kazark <kazark@zoho.com>
2016-02-18 13:25:50 -0500

    Fix smfis output logic
    Unfortunately outputting from Xml type providers was not quite as slick
    as I had hoped.

--- fsharp/smfis/smfis/Program.fs
+++ fsharp/smfis/smfis/Program.fs
@@ -1,12 +1,21 @@
 ﻿open System.IO
 open System.Text
+open System.Xml.Linq
 open FSharp.Data
 
 type private Parser = XmlProvider<"./inputexample.xml">
 
 module Sort =
     let private by (selectBy : Parser.Row -> 'a) (table : Parser.Table) =
-        Parser.Table(table.Name, table.Cols, Array.sortBy selectBy table.Rows)
+        let sorted = Array.sortBy selectBy table.Rows
+        let table = XElement(XName.Get "table", seq {
+            yield table.XElement.FirstAttribute :> XObject
+            for col in table.Cols do yield col.XElement :> XObject
+            for row in sorted do yield row.XElement :> XObject
+        })
+        let doc = XDocument(table)
+        doc.Declaration <- XDeclaration("1.0", "utf-8", "no")
+        doc
 
     let bySequence =
         by (fun row -> row.Tds.[2].Number)
@@ -17,17 +26,18 @@ module Sort =
 let usage() =
     printfn "smfis - Save Me From InstallShield!!"
     printfn "Usage:"
-    printfn "    smfis sequence <inputPath>"
+    printfn "    smfis sequence <inputPath> <outputPath>"
     printfn "        Order the XML rows sequentially (as defined by InstallShield)"
-    printfn "    smfis action <inputPath>"
+    printfn "    smfis action <inputPath> <outputPath>"
     printfn "        Order the XML rows alphabetically by action (as defined by InstallShield)"
     printfn "    smfis help"
     printfn "        Get this help message"
 
-let exec (sort : Parser.Table -> Parser.Table) inputPath =
+let exec (sort : Parser.Table -> XDocument) inputPath (outputPath : string) =
     let xml = File.ReadAllText(inputPath, Encoding.UTF8)
-    let xml' = sort <| Parser.Parse xml
-    System.Console.Write(xml')
+    let parsedXml = Parser.Parse xml
+    let sortedXml = sort parsedXml
+    sortedXml.Save(outputPath)
 
 [<EntryPoint>]
 let main argv = 
@@ -35,11 +45,11 @@ let main argv =
     | [|"help"|] -> 
         usage()
         0
-    | [|"sequence"; inputPath|] ->
-        exec Sort.bySequence inputPath
+    | [|"sequence"; inputPath; outputPath|] ->
+        exec Sort.bySequence inputPath outputPath
         0
-    | [|"action"; inputPath|] ->
-        exec Sort.byAction inputPath
+    | [|"action"; inputPath; outputPath|] ->
+        exec Sort.byAction inputPath outputPath
         0
     | _ ->
         printfn "Error: not clear what you want. Please see below."
